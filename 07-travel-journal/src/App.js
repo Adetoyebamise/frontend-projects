869734e2e5c167014ec36bf0b65bf6fb1e8e64fb
Adetoye Babalola <adetoyebamise@gmail.com>
2023-05-15 09:51:57 -0400

    add react code

--- 07-travel-journal/src/App.js
+++ 07-travel-journal/src/App.js
@@ -3,50 +3,16 @@ import World from "../src/assets/world.png"
 import LocationIcon from "../src/assets/placeholder.png"
 import Navbar from "../src/components/Navbar"
 import Place from "../src/components/Place"
+import Data from "./Data"
 
 function App() {
+  const dataSet = Data.map(itemDetails => {
+    return <Place locationpoint={LocationIcon} detail={itemDetails} />
+  })
   return (
     <div className="travel__journey__app">
       <Navbar img={World} />
-      <Place img={LocationIcon} />
-      {/* <div className="travel__japan"> */}
-      {/* <img src={LocationIcon} alt="location__icon" /> */}
-      {/* <div className="destination__description__japan">
-          <span>
-            {" "}
-            <span> </span>
-            JAPAN
-          </span>
-          <span>View on Google Maps</span>
-        </div> */}
-      {/* <div>Mount Fuji</div>
-        <div>12 Jan, 2021 - 24 Jan, 2021</div>
-        <div>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</div>
-      </div> */}
-      {/* <div className="travel__australia"> */}
-      {/* <img src={LocationIcon} alt="location__icon" /> */}
-      {/* <div className="destination__description__australia"> */}
-      {/* <span> */}
-      {/* <span> </span> AUSTRALIA */}
-      {/* </span> */}
-      {/* <span>View on Google Maps</span> */}
-      {/* </div> */}
-      {/* <div>Sydney Opera House</div> */}
-      {/* <div>27 May, 2021 - 8 Jun, 2021</div> */}
-      {/* <div>The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings</div> */}
-      {/* </div> */}
-      {/* <div className="travel__oslo"> */}
-      {/* <img src={LocationIcon} alt="location__icon" /> */}
-      {/* <div className="destination__description__oslo">
-          <span>
-            <span> </span> NORWAY
-          </span>
-          <span>View on Google Maps</span>
-        </div> */}
-      {/* <div>Geirangerfjord</div> */}
-      {/* <div>01 Oct, 2021 - 18 Nov, 2021</div> */}
-      {/* <div>The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.</div> */}
-      {/* </div> */}
+      {dataSet}
     </div>
   )
 }
--- 07-travel-journal/src/components/Place.js
+++ 07-travel-journal/src/components/Place.js
@@ -4,25 +4,25 @@ export default function place(props) {
   return (
     <div className="place__container">
       <div className="left__container">
-        <img src="https://source.unsplash.com/WLxQvbMyfas" alt="error loading" className="left__image" />
+        <img src={props.detail.imageUrl} alt="error loading" className="left__image" />
       </div>
       <div className="right__container">
         <div className="travel__location">
           <div className="travel__location__icon">
-            <img src={props.img} alt="location icon" className="travel__location__image" /> <span className="city__name"> JAPAN </span>
+            <img src={props.locationpoint} alt="location icon" className="travel__location__image" /> <span className="city__name"> {props.detail.location} </span>
           </div>
           <div className="google__map">
-            <a href="/" className="travel__location__maplink">
+            <a href={props.detail.googleMapsUrl} className="travel__location__maplink">
               View on Google Maps
             </a>
           </div>
         </div>
         <div className="travel__destination__title">
-          <h1 className="destination__name"> Mount Fuji</h1>
+          <h1 className="destination__name">{props.detail.title}</h1>
         </div>
         <div className="travel__destination__content">
-          <h4> 12 Jan, 2021 - 24 Jan, 2021</h4>
-          <p> Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+          <h4>{props.detail.startDate} - {props.detail.endDate}</h4>
+          <p> {props.detail.description}</p>
         </div>
       </div>
     </div>
